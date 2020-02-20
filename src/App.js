9ac9f9be8d1228d4317704b01028fd186f2eb468
Victor Escalona <victorescalonag@gmail.com>
2020-02-19 22:50:37 -0500

    Mobile layout done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
+  const [marvel, setMarvel] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await fetch("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=b56fcef71c17650ee98d4e32aad2416f&hash=82890dfb029d385e40ff1ec55203b80f");
+      const data = await req.json();
+      setMarvel(data);
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
+
+  if (loading) {
+    return (
+      <p>Loading...</p>
+    )
+  }
+  if (!loading) {
+    return (
+      <>
+        <div className="container">
+          {marvel.data.results.map(hero => (
+            <div className="top">
+              <p key={hero.id}>{hero.name}</p>
+            </div>
+            <div className="middle">
+              <p key={hero.id}>{hero.description}</p>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+{/*            {marvel.data.results.map(hero => (
+            <li key={hero.id}><img src={hero.thumbnail.path.concat("." + hero.thumbnail.extension)} style={{ height: "200px", width: "200px" }} alt="" /></li>
+          ))} */}
 export default App;
